6d8081645dfc5df3d54a4e888549a38886c71492
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-01 18:20:29 +0530

    finalized frontend part

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Transaction({ transaction }) {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transaction.amount < 0 ? "-" : "+";
 
   return (
@@ -9,7 +12,12 @@ export default function Transaction({ transaction }) {
       <span>
         {sign}₹{Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,9 @@
-import { createContext, useReducer } from "react";
+import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 // Initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -80 },
-    { id: 2, text: "Salary", amount: 30000 },
-    { id: 3, text: "Book", amount: -300 },
-    { id: 4, text: "Camera", amount: 15000 },
-  ],
+  transactions: [],
 };
 
 // Create context
@@ -18,10 +13,27 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
       }}
     >
       {children}
